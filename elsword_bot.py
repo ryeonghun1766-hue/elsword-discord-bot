60752231a710e4f7c79165bf68996969fce6c068
ryeonghun1766-hue <ryeonghun1766@gmail.com>
2026-09-17 02:03:27 +0900

    Add files via upload

--- elsword_bot.py
+++ elsword_bot.py
@@ -128,6 +128,12 @@ class HealthHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"Elsword Bot is running!")
 
+    # noinspection PyPep8Naming
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain; charset=utf-8")
+        self.end_headers()
+
 
 def run_health_server():
     port = int(os.environ.get("PORT", "10000"))
